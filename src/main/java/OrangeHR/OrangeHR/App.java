553a2304db584f21be5049b751ac7bacf6427a3e
Abhishek Kumar <CE512@compassitesinc.com>
2020-12-12 18:09:29 +0530

    first commit

---
+++ src/main/java/OrangeHR/OrangeHR/App.java
@@ -0,0 +1,13 @@
+package OrangeHR.OrangeHR;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
